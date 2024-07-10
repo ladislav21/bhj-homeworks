87e1dc47df0894afb02c8198a5946804f23082e1
ladislav Laptop <vladislavchs@internet.ru>
2024-07-10 23:14:40 +0300

    task 1.1 #1 done

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,29 @@
 let result = document.getElementById('timer');
 
-let newInterval = setInterval(() => {
-    if(result.textContent !== '1') {
-            result.textContent -= 1;
-        } else {
-            result.textContent -= 1;
-            clearInterval(newInterval);
-            setTimeout(() => alert('Вы выиграли в конкурсе!'));
-        }
-    }, 1000);
+let newTimer = new Date();
+
+let time = {
+    hours: 0,
+    minutes: 0,
+    seconds: parseInt(result.textContent),
+};
+
+function setNewTime(timerHours, timerMinutes, timerSeconds) {
+    newTimer.setHours(timerHours, timerMinutes, timerSeconds);
+    result.textContent = newTimer.toLocaleTimeString("ru-Ru", {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    });
+};
+
+setNewTime(time.hours, time.minutes, time.seconds);
+
+let intervalID = setInterval(() => {
+    time.seconds -= 1;
+    setNewTime(time.hours, time.minutes, time.seconds);
+    if(time.seconds === 0) {
+        clearInterval(intervalID);
+        setTimeout(() => alert('Вы выиграли в конкурсе!'));
+    }
+}, 1000);
